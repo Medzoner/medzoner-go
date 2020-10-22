724a9ee726cf3674928a93588bf669eb72a6b5da
MEdzoner <medzoner@medzoner.com>
2020-10-22 22:04:40 +0200

    complete test

--- pkg/ui/http/handler/index_handler_test.go
+++ pkg/ui/http/handler/index_handler_test.go
@@ -1,6 +1,7 @@
 package handler_test
 
 import (
+	"errors"
 	"fmt"
 	"github.com/Medzoner/medzoner-go/pkg/ui/http/handler"
 	"net/http"
@@ -16,12 +17,31 @@ func TestIndexHandler(t *testing.T) {
 		request := httptest.NewRequest("GET", "/", nil)
 		indexHandler.IndexHandle(httptest.NewRecorder(), request)
 	})
+	t.Run("Unit: test ContactHandler failed", func(t *testing.T) {
+		indexHandler := handler.IndexHandler{
+			Template: &TemplaterTestFailed{},
+		}
+		request := httptest.NewRequest("GET", "/", nil)
+
+		defer func() {
+			if r := recover(); r == nil {
+				t.Errorf("The code did not panic")
+			}
+		}()
+		indexHandler.IndexHandle(httptest.NewRecorder(), request)
+	})
+}
+
+type TemplaterTestFailed struct {
+	RootPath string
+}
+func (t *TemplaterTestFailed) Render(name string, view interface{}, response http.ResponseWriter, status int) (interface{}, error) {
+	return nil, errors.New("panic")
 }
 
 type TemplaterTest struct {
 	RootPath string
 }
-
 func (t *TemplaterTest) Render(name string, view interface{}, response http.ResponseWriter, status int) (interface{}, error) {
 	fmt.Println(view)
 	return nil, nil
