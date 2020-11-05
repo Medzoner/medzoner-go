131cbeab73273c8a47687fe3865a531152298ee1
MEdzoner <medzoner@medzoner.com>
2020-11-05 23:50:46 +0100

    fix run migration

--- cmd/migrate/migrate.go
+++ cmd/migrate/migrate.go
@@ -5,10 +5,14 @@ import (
 	"github.com/Medzoner/medzoner-go/pkg/infra/database"
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"os"
 )
 
 func main() {
-	app := &pkg.App{}
+	rootPath, _ := os.Getwd()
+	app := pkg.App{
+		RootPath: rootPath,
+	}
 	app.LoadContainer()
 	app.Container.Get("database").(*database.DbSQLInstance).CreateDatabase(false)
 	app.Container.Get("db-manager").(*database.DbMigration).MigrateUp()
