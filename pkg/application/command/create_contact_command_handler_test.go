b350aee7ba8669fce00e07eec94c522565cfbfdb
MEdzoner <medzoner@medzoner.com>
2020-11-07 23:50:12 +0100

    complete test

--- pkg/application/command/create_contact_command_handler_test.go
+++ pkg/application/command/create_contact_command_handler_test.go
@@ -34,6 +34,7 @@ func TestCreateContactCommandHandler(t *testing.T) {
 		handler.Handle(createContactCommand)
 		assert.Equal(t, loggerTest.LogMessages[0], "Contact was created.")
 		assert.Equal(t, handler.GetName(), "CreateContactCommand")
+		assert.Equal(t, createContactCommand.Email, contact.GetEmailString())
 	})
 }
 
