d641bb7709cbc55ca300913a0dfa2c08d975a47c
Medzoner <medzoner@medzoner.com>
2022-07-31 00:49:24 +0200

    fix(contact): handle when recaptcha sucess

--- pkg/ui/http/handler/contact_handler.go
+++ pkg/ui/http/handler/contact_handler.go
@@ -57,35 +57,34 @@ func (c *ContactHandler) IndexHandle(response http.ResponseWriter, request *http
 	}
 	statusCode := http.StatusOK
 	if request.Method == "POST" && request.FormValue("Envoyer") == "" {
-
 		_, buttonClicked := request.Form["button"]
 		if buttonClicked {
 			if processRequest(request) {
-				fmt.Println("Recaptcha was correct!")
+				createContactCommand := command.CreateContactCommand{
+					DateAdd: time.Now(),
+					Name:    request.FormValue("name"),
+					Email:   request.FormValue("email"),
+					Message: request.FormValue("message"),
+				}
+				v := c.Validation
+				err := v.Struct(createContactCommand)
+				if err == nil {
+					c.CreateContactCommandHandler.Handle(createContactCommand)
+					c.Session.SetValue("message", "Votre message a bien été envoyé. Merci!")
+					err = c.Session.Save(request, response)
+					if err != nil {
+						http.Error(response, err.Error(), http.StatusInternalServerError)
+						return
+					}
+					http.Redirect(response, request, "/", http.StatusSeeOther)
+					return
+				}
+				statusCode = http.StatusBadRequest
 			} else {
+				statusCode = http.StatusBadRequest
 				fmt.Println("Recaptcha was incorrect; try again.")
 			}
 		}
-		createContactCommand := command.CreateContactCommand{
-			DateAdd: time.Now(),
-			Name:    request.FormValue("name"),
-			Email:   request.FormValue("email"),
-			Message: request.FormValue("message"),
-		}
-		v := c.Validation
-		err := v.Struct(createContactCommand)
-		if err == nil {
-			c.CreateContactCommandHandler.Handle(createContactCommand)
-			c.Session.SetValue("message", "Votre message a bien été envoyé. Merci!")
-			err = c.Session.Save(request, response)
-			if err != nil {
-				http.Error(response, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			http.Redirect(response, request, "/", http.StatusSeeOther)
-			return
-		}
-		statusCode = http.StatusBadRequest
 	}
 	response.WriteHeader(statusCode)
 	_ = request
