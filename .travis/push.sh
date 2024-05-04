86e76141c2712619d4faecaf02f0222c31b22413
Mehdi Youb <mehdi@mym.fans>
2024-05-04 17:01:28 +0200

    wip3

--- .travis/push.sh
+++ .travis/push.sh
@@ -3,5 +3,5 @@
 rm .env
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 docker build -t ${DOCKER_USERNAME}/medzoner-go -f ./docker/Dockerfile .
-docker tag "${DOCKER_USERNAME}/medzoner-go" "${DOCKER_USERNAME}/medzoner-go:${TRAVIS_BRANCH}-${TRAVIS_COMMIT}"
-docker push "${DOCKER_USERNAME}/medzoner-go:${TRAVIS_BRANCH}-${TRAVIS_COMMIT}"
+docker tag "${DOCKER_USERNAME}/medzoner-go" "${DOCKER_USERNAME}/medzoner-go:${GIT_BRANCH}-${GIT_COMMIT}"
+docker push "${DOCKER_USERNAME}/medzoner-go:${GIT_BRANCH}-${GIT_COMMIT}"
