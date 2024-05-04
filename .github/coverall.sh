beec43925cbbcecdde744e8193cbbfbf0cc2eb00
Mehdi Youb <mehdi@mym.fans>
2024-05-04 18:15:47 +0200

    Fix: ci

--- .github/coverall.sh
+++ .github/coverall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if [ "${GIT_BRANCH}" = "master" ]; then
-  ${HOME}/gopath/bin/goveralls -coverprofile=coverage.out -service=travis-ci -repotoken "${COVERALLS_TOKEN}";
+  $GOPATH/bin/goveralls -coverprofile=coverage.out -service=travis-ci -repotoken "${COVERALLS_TOKEN}";
 fi
