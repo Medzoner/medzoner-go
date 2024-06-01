27912e8a8481a7bd6d25976cf91042158430dd79
Mehdi Youb <medzoner@medzoner.com>
2024-06-01 15:41:48 +0200

    Fix: cd push registry script (#44)
    Co-authored-by: Mehdi Youb <mehdi@mym.fans>

--- .github/push.sh
+++ .github/push.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 rm .env
+
+REPOSITORY="${DOCKER_USERNAME}/medzoner-go"
+
+echo "Registry login..."
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
-docker build -t ${DOCKER_USERNAME}/medzoner-go -f ./docker/Dockerfile .
-docker tag "${DOCKER_USERNAME}/medzoner-go" "${DOCKER_USERNAME}/medzoner-go:${GIT_BRANCH}-${GIT_COMMIT}"
-docker push "${DOCKER_USERNAME}/medzoner-go:${GIT_BRANCH}-${GIT_COMMIT}"
+
+echo "Building image ${REPOSITORY} ..."
+docker build -t ${REPOSITORY} -f ./docker/Dockerfile .
+
+echo "Tag image ${REPOSITORY_NEW} ..."
+REPOSITORY_NEW="${REPOSITORY}:${GIT_BRANCH}-${GIT_COMMIT}"
+docker tag "${REPOSITORY}" "${REPOSITORY_NEW}"
+
+echo "Pushing image to registry ${REPOSITORY_NEW} ..."
+docker push "${REPOSITORY_NEW}"
