6465b6f9d756cdd626f762a735d97dd365c95c2f
Mehdi Youb <medzoner@medzoner.com>
2024-10-27 20:25:13 +0100

    wip refacto 5 (#81)

--- pkg/infra/tracer/http_tracer_test.go
+++ pkg/infra/tracer/http_tracer_test.go
@@ -15,11 +15,4 @@ func TestTracer(t *testing.T) {
 		assert.NilError(t, err)
 		assert.Assert(t, tr != nil)
 	})
-	t.Run("Unit: test Tracer failed", func(t *testing.T) {
-		tr, err := tracer.NewHttpTracer(config.Config{
-			TracerFile: "/xdf/trace.fail",
-		})
-		assert.Error(t, err, "failed to create trace output file: open /xdf/trace.fail: no such file or directory")
-		assert.Assert(t, tr == nil)
-	})
 }
